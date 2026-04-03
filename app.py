94673ec1dd3cb14adf8758e1288221aab728ee48
Krrish Mantri <krinja.krrish1306@gmail.com>
2026-04-03 22:13:56 +0530

    Remove unused TensorFlow imports from app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ import yfinance as yf
 
 # 🔥 AI IMPORTS
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+
 
 app = FastAPI()
 
